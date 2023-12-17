790afba043b2ac96ba5e7a870d4581217b13f571
Andreas Ernst <andreas.ernst7@gmail.com>
2023-12-17 16:45:40 +0100

    partial is dirty handling

--- web/src/app/portal/microfrontend-details.component.ts
+++ web/src/app/portal/microfrontend-details.component.ts
@@ -6,7 +6,7 @@ import { Subscription } from 'rxjs';
 import { MirofrontendsComponent } from "./microfrontends.component";
 import { EditorModel } from "../widgets/monaco-editor/monaco-editor";
 import { v4 as uuidv4 } from 'uuid'
-import { NgForm } from "@angular/forms";
+import { FormBuilder, FormGroup, NgForm } from "@angular/forms";
 @Component({
   selector: 'microfrontend-details',
   templateUrl: './microfrontend-details.component.html',
@@ -49,13 +49,47 @@ export class MicrofrontendDetailsComponent implements OnInit, OnDestroy {
   allCategories: string[] = [];
   allVisibilities: string[] = ['public', 'private'];
 
+  formGroup: FormGroup
+
+    dirty = {}
+    isDirty = false
+
   // constructor
 
-  constructor(private activatedRoute: ActivatedRoute, private microfrontendsComponent: MirofrontendsComponent) {
+  constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private microfrontendsComponent: MirofrontendsComponent) {
     microfrontendsComponent.pushRouteElement(this.element)
+
+      this.formGroup = this.formBuilder.group({
+          id: [''],
+          visibility: [[]],
+          permissions: [[]],
+          categories: [[]],
+          tags: [[]]
+          }
+      )
   }
 
   // public
+
+    save() {
+      // copy values from
+
+        for ( let propertyName in this.dirty)
+            this.selectedFeature[propertyName] = this.dirty[propertyName]
+
+        // save
+
+        this.microfrontendsComponent.saveManifest(this.manifest)
+
+        // new state
+
+        this.selectFeature(this.selectedFeature)
+    }
+
+    revert() {
+      // will trigger isDirty calculation
+        this.selectFeature(this.selectedFeature)
+    }
 
     setManifests(manifests: Manifest[]):Manifest[] {
       // local functions
@@ -104,6 +138,41 @@ export class MicrofrontendDetailsComponent implements OnInit, OnDestroy {
 
   selectFeature(feature: Feature) {
     this.selectedFeature = feature
+
+      this.formGroup.setValue({
+          id: feature.id,
+          visibility: feature.visibility,
+          categories: feature.categories,
+          tags: feature.tags,
+          permissions: feature.permissions
+      })
+  }
+
+  private checkChanges(value: any) {
+      const equalArrays = (a, b) => {
+        if ( a == undefined || b == undefined)
+          return a == b
+
+          if ( a.length != b.length )
+              return false
+
+           let sorted = b.sort()
+          return a.sort().every((element, index) => element == sorted[index]);
+    }
+
+      const equals = (a, b) => Array.isArray(a) ? equalArrays(a, b) : a == b
+
+      let dirty = {}
+
+      this.isDirty = false
+      for (let propertyName in value) {
+          if (!equals(this.selectedFeature[propertyName], value[propertyName])) {
+              dirty[propertyName] = value[propertyName]
+              this.isDirty = true
+          }
+      }
+
+      this.dirty = dirty
   }
 
   // implement OnInit
@@ -112,6 +181,8 @@ export class MicrofrontendDetailsComponent implements OnInit, OnDestroy {
     this.subscription = this.activatedRoute.params.subscribe(params => {
       this.setManifest(params["microfrontend"])
     })
+
+      this.formGroup.valueChanges.subscribe(value => this.checkChanges(value))
   }
 
   // implement OnDestroy
