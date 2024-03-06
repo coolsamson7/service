f2852f095d5e77690df33f9dec828cfcdc58ba0a
Andreas Ernst <andreas.ernst7@gmail.com>
2024-03-06 21:24:06 +0100

    css

--- modulefederation/apps/web/src/app/portal/config/string/string-parameter.component.ts
+++ modulefederation/apps/web/src/app/portal/config/string/string-parameter.component.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @angular-eslint/no-output-on-prefix */
 /* eslint-disable @angular-eslint/no-outputs-metadata-property */
 import { CommonModule } from "@angular/common"
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit, ViewEncapsulation } from "@angular/core"
 import { FormsModule } from "@angular/forms"
 import { MatInputModule } from "@angular/material/input"
 import { ParamComponent } from "../parameter-component"
@@ -12,7 +12,8 @@ import { ParamComponent } from "../parameter-component"
   imports: [CommonModule, FormsModule, MatInputModule],
   selector: 'string-parameter',
   templateUrl: './string-parameter.component.html',
-  outputs: ["onChange"]
+  outputs: ["onChange"],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class StringParamComponent extends ParamComponent implements OnInit  {
 
