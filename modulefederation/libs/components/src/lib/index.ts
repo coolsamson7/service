92c5ec4ca689035d69c730f35fe003f7cb93f561
Andreas Ernst <andreas.ernst7@gmail.com>
2024-10-30 12:36:41 +0100

    added web component wrapper

--- modulefederation/libs/components/src/lib/index.ts
+++ modulefederation/libs/components/src/lib/index.ts
@@ -4,3 +4,4 @@ export * from './ui/resize-observable';
 export * from './ui/icon.component';
 export * from './cdk-connected-overlay-backdrop';
 export * from './monaco-editor';
+export * from './web-component';
---
+++ modulefederation/libs/components/src/lib/web-component/web-component-wrapper.ts
@@ -0,0 +1,77 @@
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { loadRemoteModule, setRemoteDefinitions } from '@nx/angular/mf';
+
+export type WebComponentWrapperOptions =  {
+  elementName: string;
+};
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'wc-wrapper',
+  template: '<div #vc></div>',
+})
+// eslint-disable-next-line @angular-eslint/component-class-suffix
+export class WebComponentWrapper implements AfterContentInit, OnChanges {
+  // input
+
+  @Input() options: WebComponentWrapperOptions | null = null;
+  @Input() props: { [prop: string]: unknown } = {};
+  @Input() events: { [event: string]: (event: Event) => void } = {};
+
+  // instance data
+
+  @ViewChild('vc', { read: ElementRef, static: true })
+  vc!: ElementRef;
+  element!: HTMLElement;
+
+  // constructor
+
+  constructor(private route: ActivatedRoute) {}
+
+  // implement OnChanges
+
+  ngOnChanges(): void {
+    console.log("jjj")
+    if (this.element)
+      this.populateProps();
+  }
+
+  // implement AfterContentInit
+
+  async ngAfterContentInit() {
+    const options = this.options ?? (this.route.snapshot.data as WebComponentWrapperOptions);
+
+    try {
+      await loadRemoteModule("react", "./Module"); // TODO
+
+      this.element = document.createElement(options.elementName);
+      this.populateProps();
+      this.setupEvents();
+
+      this.vc.nativeElement.appendChild(this.element);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  // private
+
+  private populateProps() {
+    for (const prop in this.props)
+      (this.element as any)[prop] = this.props[prop];
+  }
+
+  private setupEvents() {
+    for (const event in this.events)
+      this.element.addEventListener(event, this.events[event]);
+  }
+}
